ee57e557ae40d68fd12df29eef18612a4f2c4a6f
Robert Wenzel <robertwenzel@potix.com>
2020-01-14 18:47:52 +0800

    live reload don't reload external style sheets

--- src/liveReload/templates/zklessLiveReloadStyles.js
+++ src/liveReload/templates/zklessLiveReloadStyles.js
@@ -2,7 +2,9 @@
     var socket = io.connect('http://localhost:{{port}}');
     socket.on("zkless-success", function () {
         document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
-            link.href = link.href.replace(/\?.*|$/, '?t=' + Date.now());
+            if(link.href.indexOf(window.location.origin) === 0) {
+                link.href = link.href.replace(/\?.*|$/, '?t=' + Date.now());
+            }
         });
     });
     socket.on("zkless-failure", function () {
